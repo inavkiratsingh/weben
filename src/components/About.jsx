f7b9c048ec47f4a3c52995fe82a4daff2fd3e7c9
inavkiratsingh <navs6164@gmail.com>
2024-06-24 19:27:40 +0530

    again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-
+import Image from 'next/image';
 import React from 'react'
 import { HiArrowLongRight } from "react-icons/hi2";
 
@@ -10,7 +10,13 @@ function About() {
         <div className='w-full flex justify-center p-20'>
             <div className="left-about min-w-[50%]">
                 <div className="img w-[35rem] h-[40rem] ml-[130px] mb-44">
-                    <img className='w-full h-full object-cover' src="/images/about/about-l.webp" alt="" />
+                <Image
+                    className='object-cover w-full h-full' 
+                    src="/images/about/about-l.webp" 
+                    alt="image"
+                    width= {500}
+                    height={500}
+                    objectFit='cover' />
                 </div>
                 <p className='w-64 grotesk text-[1.23rem] ml-[400px] leading-8'>
                     At <strong>Weben</strong>, we’re all about <strong>action</strong>. What are the specific <strong>actions</strong> that fuel your company's growth? Do you need more consumers to <strong>buy</strong> your product? Want more people to <strong>subscribe</strong> to your list, <strong>download</strong> your app, or <strong>learn</strong> about your brand? Or perhaps your goal is simply to <strong>generate</strong> more leads for your products and services? Whatever <strong>actions</strong> you're targeting, <strong>Vrrb</strong> can help.
@@ -18,7 +24,13 @@ function About() {
             </div>
             <div className="right-about min-w-[50%] pt-60 relative" data-scroll-container>
                 <div className="img w-[25rem] h-[26rem] ml-28 mt-60 mb-28 z-10" data-scroll data-scroll-speed=".15">
-                    <img className='w-full h-full object-cover' src="/images/about/about-r.webp" alt="" />
+                    <Image
+                    className='object-cover' 
+                    src="/images/about/about-r.webp" 
+                    alt="image"
+                    width= {500}
+                    height={500}
+                    objectFit='cover' />
                 </div>
                 <div className="absolute circle bg-[#FF5E5E] w-72 h-72 rounded-full z-20 top-72" data-scroll data-scroll-speed="-.05"></div>
                 <p className='w-[500px] text-7xl grotesk leading-[5.3rem]'>
