b7c027bfda09de1e53a8200af662e1f34fa396c3
Ubuntu <navs6164@gmail.com>
2024-07-05 19:38:24 +0530

    main

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,8 @@ import ParallaxText from './ui/ParallexText'
 function Projects() {
     return (
         <section className="w-full h-screen overflow-hidden text-center">
-            <ParallaxText baseVelocity={-10}>Featured projects</ParallaxText>
-            <ParallaxText baseVelocity={10}>Featured projects</ParallaxText>
+            {/* <ParallaxText baseVelocity={-10}>Featured projects</ParallaxText>
+            <ParallaxText baseVelocity={10}>Featured projects</ParallaxText> */}
         </section>
     )
 }
